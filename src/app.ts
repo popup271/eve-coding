fdfca5131e1418b1a76e7a31d2071f9b1c819902
Nick <popup271gaming@gmail.com>
2022-08-28 21:11:08 -0400

    Contracts by Region

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,44 @@
-import express from 'express';
-const app = express();
-const port = 3000;
+import { json } from 'express';
+import sqlite3 from 'sqlite3';
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
+const REGIONS_URL = "https://esi.evetech.net/latest/universe/regions/?datasource=tranquility"
 
-app.listen(port, () => {
-  return console.log(`Express is listening at http://localhost:${port}`);
-});
+async function getEsiURL(url: string) {
+  const response = await fetch(url);
+  const json = await response.json();
+  return json;
+}
+
+async function getEsiURLHeader(url: string) {
+  const response = await fetch(url);
+  const headers = response.headers.get('x-pages');
+
+  return headers;
+
+}
+
+async function getContracts(region: string) {
+  const url = "https://esi.evetech.net/latest/contracts/public/" + region + "/?datasource=tranquility&page=1"
+  const pageCount = getEsiURLHeader(url);
+  const pageInt = parseInt(await pageCount);
+
+  for (let i = 1; i <= pageInt; i++) {
+    const urlPages = "https://esi.evetech.net/latest/contracts/public/" + region + "/?datasource=tranquility&page=" + i
+    console.log(urlPages)
+    const respone = await fetch(urlPages);
+    const json = await respone.json();
+    console.log(json);
+    console.log(i);
+  }
+}
+
+
+
+async function main() {
+  const regions: number[] = await getEsiURL(REGIONS_URL);
+  console.log(regions);
+  await getContracts("10000043");
+
+}
+
+main();
